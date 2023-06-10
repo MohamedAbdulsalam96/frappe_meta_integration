200ebcbc369c40c2893869114e0d122fa9134f6d
SherinKR <sherinkrply@gmail.com>
2023-06-10 11:06:43 +0530

    test

--- frappe_meta_integration/whatsapp/api/webhook.py
+++ frappe_meta_integration/whatsapp/api/webhook.py
@@ -5,18 +5,19 @@ import random
 
 @frappe.whitelist(allow_guest=True)
 def handle():
-	if frappe.request.method == "GET":
-		return verify_token_and_fulfill_challenge()
+    if frappe.request.method == "GET":
+        return verify_token_and_fulfill_challenge()
 
     try:
-		form_dict = frappe.local.form_dict
+        form_dict = frappe.local.form_dict
         res = ''.join(random.choices(string.ascii_lowercase + string.digits, k=8))
-		frappe.get_doc(
-			{"doctype": "Note", "title":res, "content": frappe.as_json(form_dict)}
-		).insert(ignore_permissions=True)
-	except Exception:
-		frappe.log_error("WhatsApp Webhook Log Error", frappe.get_traceback())
-		frappe.throw("Something went wrong")
+        frappe.get_doc(
+            {"doctype": "Note", "title":res, "content": frappe.as_json(form_dict)}
+        ).insert(ignore_permissions=True)
+        frappe.db.commit()
+    except Exception:
+        frappe.log_error("WhatsApp Webhook Log Error", frappe.get_traceback())
+        frappe.throw("Something went wrong")
 
 def verify_token_and_fulfill_challenge():
 	meta_challenge = frappe.form_dict.get("hub.challenge")
