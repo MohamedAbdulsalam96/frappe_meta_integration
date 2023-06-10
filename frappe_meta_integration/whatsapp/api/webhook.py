607dd18f0dd7b694eab750311775b9144e6e5c13
SherinKR <sherinkrply@gmail.com>
2023-06-10 10:33:42 +0530

    webhook testing

---
+++ frappe_meta_integration/whatsapp/api/webhook.py
@@ -0,0 +1,16 @@
+import frappe
+from werkzeug.wrappers import Response
+
+@frappe.whitelist(allow_guest=True)
+def handle():
+	if frappe.request.method == "GET":
+		return verify_token_and_fulfill_challenge()
+
+def verify_token_and_fulfill_challenge():
+	meta_challenge = frappe.form_dict.get("hub.challenge")
+	expected_token = frappe.db.get_single_value("WhatsApp Cloud API Settings", "webhook_verify_token")
+
+	if frappe.form_dict.get("hub.verify_token") != expected_token:
+		frappe.throw("Verify token does not match")
+
+	return Response(meta_challenge, status=200)
